b80bd0374c467ae1305825101c185b671e8dd113
panxinming <pzrr@qq.com>
2020-01-04 17:18:36 +0800

    init version

--- src/protected/const/const.go
+++
@@ -1,3 +0,0 @@
-package _const
-
-const ()
--- src/protected/model/TaskInfo.go
+++ src/protected/model/TaskInfo.go
@@ -2,13 +2,12 @@ package model
 
 type DemoInfo struct {
 	ID         int
-	NodeID     string
 	TargetType string //http/shell/goso
-	Target     string
 	TaskID     string
 	TaskType   string
 	IsRun      bool
 	DueTime    int64
 	Interval   int64
 	Express    string
+	TaskData   string
 }
