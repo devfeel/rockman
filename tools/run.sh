f037f09699e4c0ef703b6e60cc472bd77530cf41
panxinming <pzrr@qq.com>
2020-04-25 22:36:03 +0800

    add plugin shell

--- tools/run.sh
+++ tools/run.sh
@@ -1,2 +1,4 @@
 master1: nohup ./rockman -outerhost=116.62.16.66 -cluster=rock &
 master2: nohup ./rockman -outerhost=118.31.32.168 -cluster=rock &
+
+plugin: go build -buildmode=plugin -o plugin.so plugin.go
